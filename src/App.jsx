49d575e8eb3d4c19e61a5410979fe7fcbc7ae8f3
Rodney Moore <rlamar1977@gmail.com>
2026-03-15 19:11:48 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import MinimumPayments from './pages/MinimumPayments';
+import CreditUtilization from './pages/CreditUtilization';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
